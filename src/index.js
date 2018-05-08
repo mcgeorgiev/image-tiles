8dd045a768e61791fed32ddd077f0d53065376b8
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2018-05-08 17:15:14 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tile extends React.Component {
+  render() {
+    return (
+     <img src="http://via.placeholder.com/300x300" alt="placeholder image" />
+    );
+  }
+}
+
+class Container extends React.Component {
+  render() {
+    let tiles = [];
+    for (let i = 0; i < 9; i++) {
+      tiles.push(
+        <Tile />
+      );
+    }
+    
+    return (
+      <div className="tiles">{tiles}</div>
+    );
+
+  }
+};
+
+ReactDOM.render(
+  <Container />,
+  document.getElementById('root')
+);
