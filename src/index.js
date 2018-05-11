a56537fe3887a895418f50bd2fa80873b3c9f918
mcgeorgiev <michael_georgiev@hotmail.co.uk>
2018-05-11 16:43:36 +0100

    working onClick for components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import classNames from 'classnames';
 class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({expanded : !this.state.expanded});
+    console.log(this.state.expanded);
+  }
+
   render() {
+    let classes = classNames({
+      'tile' : true,
+      'expanded': this.state.expanded 
+    });
     return (
-     <img src="http://via.placeholder.com/300x300" alt="placeholder image" />
+      <div className={classes} onClick={this.handleClick} >
+        <img src="http://via.placeholder.com/300x300" alt="placeholder image" />
+      </div>
+    );
+  }
+}
+class ExpandedTile extends React.Component {
+  render(props) {
+    let classes = classNames({
+      'expanded-tile': true,
+      'open': this.props.expanded
+    });
+    return (
+      <div className='expanded-tile'>
+        <p>This is the expanded Tile</p>
+      </div>
     );
   }
 }
@@ -14,12 +46,15 @@ class Container extends React.Component {
     let tiles = [];
     for (let i = 0; i < 9; i++) {
       tiles.push(
-        <Tile />
+        <Tile key={i} />
       );
     }
     
     return (
-      <div className="tiles">{tiles}</div>
+      <div>
+        <ExpandedTile />
+        <div className="tiles">{tiles}</div>
+      </div>
     );
 
   }
